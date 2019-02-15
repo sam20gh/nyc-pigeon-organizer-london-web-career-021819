384c378e40ab8ce1d6d06ced3da24c8a2946ca35
sam ghoreishi <sam20gh@gmail.com>
2019-02-15 13:57:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,13 +2,34 @@ require 'pry'
 
 
 def nyc_pigeon_organizer(data)
-new_hash = {}
-
-   data.each do |attribute_category, attribute_data_hash|
+data.each do |attribute_category, attribute_data_hash|
     attribute_data_hash.each do |attribute, name_array|
       name_array.each do |name|
         new_pigeon_data[name] = {:color => [], :gender => [], :lives => []}  #establish top tier
       end
     end
   end
+  data[:color].each do |attribute, name_array|
+    name_array.each do |name|
+      if data[:color][attribute].include?(name)
+        new_pigeon_data[name][:color] << attribute.to_s
+      end
+    end
+  end
+  data[:gender].each do |attribute, name_array|
+    name_array.each do |name|
+      if data[:gender][attribute].include?(name)
+        new_pigeon_data[name][:gender] << attribute.to_s
+      end
+    end
+  end
+  data[:lives].each do |attribute, name_array|
+    name_array.each do |name|
+      if data[:lives][attribute].include?(name)
+        new_pigeon_data[name][:lives] << attribute
+      end
+    end
+  end
+  binding.pry
+  new_pigeon_data
 end
