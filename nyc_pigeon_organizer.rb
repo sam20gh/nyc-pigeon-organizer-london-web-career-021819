4c2d774bc53aaa35736071caa566658f27375b3c
sam ghoreishi <sam20gh@gmail.com>
2019-02-15 13:49:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
    data.each do |attribute_category, attribute_data_hash|
     attribute_data_hash.each do |attribute, name_array|
       name_array.each do |name|
-        new_pigeon_data[name] = {:color => [], :gender => [], :lives => []} 
+        new_pigeon_data[name] = {:color => [], :gender => [], :lives => []}  #establish top tier
       end
     end
-end
+  end
